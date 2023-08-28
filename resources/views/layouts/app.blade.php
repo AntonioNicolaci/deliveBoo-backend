28c3df8635195a5fd1fc7134c178d6cda6de0aa5
antonionicolaci <antonionicolaci48@gmail.com>
2023-08-28 10:10:08 +0200

    re-add user dropdown

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -69,6 +69,24 @@
                                     </a>
                                 </li>
                             @endif
+                            <li class="nav-item dropdown">
+                            <a id="navbarDropdown" class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-haspopup="true" aria-expanded="false" v-pre>
+                                {{ Auth::user()->name }}
+                            </a>
+
+                            <div class="dropdown-menu dropdown-menu-right" aria-labelledby="navbarDropdown">
+                                <a class="dropdown-item" href="{{ url('dashboard') }}">{{__('Dashboard')}}</a>
+                                <a class="dropdown-item" href="{{ url('profile') }}">{{__('Profile')}}</a>
+                                <a class="dropdown-item" href="{{ route('logout') }}" onclick="event.preventDefault();
+                                                     document.getElementById('logout-form').submit();">
+                                    {{ __('Logout') }}
+                                </a>
+
+                                <form id="logout-form" action="{{ route('logout') }}" method="POST" class="d-none">
+                                    @csrf
+                                </form>
+                            </div>
+                        </li>
                         @endauth
                         <li class="nav-item">
                             <a class="nav-link" href="{{ route('dashboard') }}">
@@ -76,6 +94,7 @@
                                 {{-- <x-buttonNav :message="Homepage"> --}}
                             </a>
                         </li>
+                        
                     </ul>
                 </div>
             </div>
