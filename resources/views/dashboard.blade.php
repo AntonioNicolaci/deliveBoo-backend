645bc6bf9527a18ee143058cb464e205ae38463b
vincenzodivita <divitavincenzo@icloud.com>
2023-08-29 09:44:22 +0200

    Add division to price

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -40,7 +40,7 @@
             <th scope="col">Nome</th>
             <th scope="col">Ingredienti</th>
             <th scope="col">Prezzo</th>
-            <th scope="col">Visibilità</th>
+            <th scope="col">Visibile</th>
           </tr>
         </thead>
         <tbody>
@@ -49,7 +49,7 @@
             <tr>
                 <td>{{$plate->name}}</td>
                 <td>{{$plate->ingredients}}</td>
-                <td>{{$plate->price}}</td>
+                <td>€ {{number_format($plate->price/100, 2, ',', '')}}</td>
                 <td>{{$plate->visibility ? 'Sì' : 'No'}}</td>
               </tr>
             @endforeach
