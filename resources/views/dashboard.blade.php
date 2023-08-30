d599fd12ef4d4935be4c0975c7dfffe85c18228d
VincenzoMinardi <vincenzo.minardi.mv@gmail.com>
2023-08-30 16:02:22 +0200

    add comment in table

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -41,8 +41,8 @@
             <th scope="col">Ingredienti</th>
             <th scope="col">Prezzo</th>
             <th scope="col">Visibile</th>
-            <th></th>
-            <th></th>
+            <th scope="col"></th>   {{-- Bottone modifica --}}
+            <th scope="col"></th>   {{-- Bottone elimina --}}
           </tr>
         </thead>
         <tbody>
